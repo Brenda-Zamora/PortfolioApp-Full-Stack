2d08e6a65f854b91f3c3ee49ddab3c380e38eac5
Brenda-Zamora <bzamoral@my.centennialcollege.ca>
2025-07-30 20:44:41 -0400

    Fix API route paths and prepare for full deployment

--- server/routes/projects.routes.js
+++ server/routes/projects.routes.js
@@ -5,13 +5,13 @@ import authCtrl from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router
-  .route("/api/projects")
+  .route("/")
   .get(projCtrl.getAllProjects)
   .post(authCtrl.requireSignin, authCtrl.isAdmin, projCtrl.addProject) // just admin can add a project
   .delete(authCtrl.requireSignin, authCtrl.isAdmin, projCtrl.deleteAllProjects); // just admin can delete all
 
 router
-  .route("/api/projects/:projectId")
+  .route("/:projectId")
   .get(projCtrl.getProjectById) // anyone can get a project by ID
   .put(authCtrl.requireSignin, authCtrl.isAdmin, projCtrl.updateProject) // just admin can update
   .delete(authCtrl.requireSignin, authCtrl.isAdmin, projCtrl.deleteProjectById); // just admin can delete
